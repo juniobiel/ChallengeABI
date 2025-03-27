68b3c5df5c7859934d78915fb374db06f160749f
Gabriel Júnio <gabriel.junio@rocketmail.com>
2025-03-27 13:35:25 -0300

    Update Program.cs

--- Server.WebSocket/Program.cs
+++ Server.WebSocket/Program.cs
@@ -1,26 +1,49 @@
 using WS.Server.Configurations;
-using WS.Server.Services;
+//using WS.Server.Services;
+using Server.WebSocket.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddInjections();
 
-var app = builder.Build();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin", policy =>
+    {
+        policy.WithOrigins("http://localhost:8000") // Substitua pela URL do cliente
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .SetIsOriginAllowed((host) => true) // Adicional: Permite subdomínios ou origens dinâmicas
+              .AllowCredentials(); // Apenas quando necessário
+    });
 
-app.UseWebSockets();
+});
 
-app.MapGet("/", async ( HttpContext context, ChatService chatService ) =>
+builder.Services.AddSignalR();
+
+WebApplication app = builder.Build();
+
+app.UseCors("AllowSpecificOrigin");
+
+app.MapHub<ChatHub>("/chathub");
+
+//app.UseWebSockets();
+
+//TODO: SignalR, HUB
+/* app.MapGet("/", async ( HttpContext context, ChatService chatService ) =>
 {
     if (context.WebSockets.IsWebSocketRequest)
     {
         var webSocket = await context.WebSockets.AcceptWebSocketAsync();
-        await chatService.HandleWebSocketConnection(webSocket);
+        await chatService.HandleWebSocketConnection(webSocket);       
     }
     else
     {
         context.Response.StatusCode = 400;
         await context.Response.WriteAsync("Expected a WebSocket request");
     }
-});
+}); */
+
 
 await app.RunAsync();
+
